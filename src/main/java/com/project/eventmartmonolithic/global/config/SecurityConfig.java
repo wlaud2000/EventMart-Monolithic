df805cc44be8248a9468049bbfa47421bec04ce5
Jimyeong Kim <kjm021221@icloud.com>
2025-03-02 12:58:18 +0900

    :sparkles:feat: CorsConfig 추가 및 SecurityConfig 수정

--- src/main/java/com/project/eventmartmonolithic/global/config/SecurityConfig.java
+++ src/main/java/com/project/eventmartmonolithic/global/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -36,14 +38,35 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        // CORS 정책 설정
         http
-                .csrf((auth) -> auth.disable());
+                .cors(cors -> cors
+                        .configurationSource(CorsConfig.apiConfigurationSource()));
 
+        // csrf 비활성화
         http
-                .authorizeHttpRequests((auth) -> auth.anyRequest().authenticated());
+                .csrf(AbstractHttpConfigurer::disable);
 
+        // form 로그인 방식 비활성화 -> REST API 로그인을 사용할 것이기 때문에
         http
-                .httpBasic(Customizer.withDefaults());
+                .formLogin(AbstractHttpConfigurer::disable);
+
+        // http basic 인증 방식 비활성화
+        http
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        // 세션을 사용하지 않음. (세션 생성 정책을 Stateless 설정.)
+        http
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // 경로별 인가
+        http
+                .authorizeHttpRequests(auth -> auth
+                        //위에서 정의했던 allowedUrls 들은 인증이 필요하지 않음 -> permitAll
+                        .requestMatchers(allowedUrls).permitAll()
+                        .anyRequest().authenticated() // 그 외의 url 들은 인증이 필요함
+                );
 
 
         return http.build();
