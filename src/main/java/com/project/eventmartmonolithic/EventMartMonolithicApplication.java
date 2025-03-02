4139cf26c3d209faac226db1ae3591ee6cc1629d
Jimyeong Kim <kjm021221@icloud.com>
2025-03-02 12:41:39 +0900

    :sparkles:feat: BaseEntity 생성

--- src/main/java/com/project/eventmartmonolithic/EventMartMonolithicApplication.java
+++ src/main/java/com/project/eventmartmonolithic/EventMartMonolithicApplication.java
@@ -2,8 +2,10 @@ package com.project.eventmartmonolithic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing //JPA Auditing 기능 활성화
 public class EventMartMonolithicApplication {
 
     public static void main(String[] args) {
